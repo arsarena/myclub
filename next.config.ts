48a882ce2bc96e2e7930f1b1970ae0e3a3f8c34a
Jeshwanthbiradar <jeshwanthbiradar@gmail.com>
2026-06-17 14:36:48 +0530

    feat: allow image optimization from Vercel blob storage domain

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "jzyg3qcwdokpd0fx.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
